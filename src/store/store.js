e00ead5e498a5f1a6e862a99d977f328bfe733f2
Long Zhao <zlong@outlook.com>
2020-07-02 13:24:19 +1000

    🐛 Fixing a bug we react redux dev tool is not defined

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ const reducer = combineReducers({
 
 const store = createStore(reducer, {}, compose(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
 ));
 
 export default store;
